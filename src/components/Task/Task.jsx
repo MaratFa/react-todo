a511b9006615845ff4a5e0472ef24cf6c8b06ccb
MaratFa <evolution99@rambler.ru>
2025-08-29 00:05:35 +0300

    edited

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,15 +1,15 @@
 import classes from "./Task.module.css";
 import { Checkbox } from "@mui/material";
 
-export default function Task({ title, description }) {
+export default function Task({ task }) {
   return (
     <div className={classes.task}>
       <div className={classes["task-check"]}>
         <Checkbox />
       </div>
       <div className={classes["task-info"]}>
-        <h2 className={classes["task-info__title"]}>{title}</h2>
-        <p className={classes["task-info__description"]}>{description}</p>
+        <h2 className={classes["task-info__title"]}>{task.title}</h2>
+        <p className={classes["task-info__description"]}>{task.description}</p>
       </div>
       <div className={classes["task-actions"]}></div>
     </div>

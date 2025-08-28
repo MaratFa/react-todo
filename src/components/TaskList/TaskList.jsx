3a6a7a74c2b2f05bc2ade28caa94f1ccb3496823
MaratFa <evolution99@rambler.ru>
2025-08-29 00:05:37 +0300

    edited

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,14 +1,18 @@
 import Task from "../Task/Task";
 import classes from "./TaskList.module.css";
 
-export default function TaskList({ tasks, title }) {
+export default function TaskList({ title, tasks }) {
+  console.log("Tasks: ", tasks);
+
   return (
     <div className={classes.wrapper}>
       <div className={classes["list-title"]}>{title}</div>
       <div className={classes["task-list"]}>
-        {/* tasks */}
-        <Task />
-        <Task />
+        {tasks.length
+          ? tasks.map((task) => {
+              return <Task task={task} key={task.id} />;
+            })
+          : "The task list is empty"}
       </div>
     </div>
   );

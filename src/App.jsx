d33924d4a1f9570d59cd9712b3d8812c65614494
MaratFa <evolution99@rambler.ru>
2025-08-29 00:05:23 +0300

    edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
+import TaskList from "./components/TaskList/TaskList";
+import { useState } from "react";
 import "./App.css";
-import Task from "./components/Task/Task";
+import AddBtn from "./components/AddBtn/AddBtn";
 
 function App() {
+  const [tasks, setTask] = useState([
+    { id: 1, title: "Task 1", description: "Task 1 description", done: false },
+    { id: 2, title: "Task 2", description: "Task 2 description", done: false },
+    { id: 3, title: "Task 3", description: "Task 3 description", done: false },
+    { id: 4, title: "Task 4", description: "Task 4 description", done: false },
+  ]);
+
   return (
-    <div>
-      <Task title="Task 1" description="Task 1 description"/>
-      <Task title="Task 2" description="Task 2 description"/>
-      <Task title="Task 3" description="Task 3 description"/>
-      <Task title="Task 4" description="Task 4 description"/>
-    </div>
+    <>
+      <div className="main-wrapper">
+        <TaskList title="Active" tasks={tasks} />
+        <TaskList title="Completed" tasks={tasks} />
+        <AddBtn />
+      </div>
+    </>
   );
 }
 

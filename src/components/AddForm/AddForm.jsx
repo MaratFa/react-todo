92d485903a5667f489deee872018a2969a439a13
MaratFa <evolution99@rambler.ru>
2025-09-01 00:27:09 +0300

    edited

--- src/components/AddForm/AddForm.jsx
+++ src/components/AddForm/AddForm.jsx
@@ -1,33 +1,52 @@
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import classes from "./AddForm.module.css";
 
-export default function AddForm() {
+export default function AddForm({ addTask }) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [isTitleValid, setIsTitleValid] = useState(title && title.trim());
+  const [isDescriptionValid, setIsDescriptionValid] = useState(
+    description && description.trim()
+  );
+
+  useEffect(() => {
+    setIsTitleValid(title && title.trim());
+    setIsDescriptionValid(description && description.trim());
+  }, [title, description]);
 
   const addNewTask = (e) => {
+    e.preventDefault();
 
-  }
+    if (!isTitleValid || !isDescriptionValid) return;
+
+    addTask();
+
+    console.log("New task was added");
+  };
 
   return (
     <>
-      <form 
+      <form
         className={classes["add-task-form"]}
-        onSubmit={(e) => addNewTask()}
+        onSubmit={(e) => addNewTask(e)}
       >
         <h2>New task</h2>
         <div className={classes["form-title"]}>
           <TextField
+            error={!isTitleValid}
             value={title}
+            helperText={!isTitleValid && "This field is required"}
             label="Name"
             onChange={(e) => setTitle(e.target.value)}
           />
         </div>
         <div className={classes["form-description"]}>
           <TextField
+            error={!isDescriptionValid}
             value={description}
+            helperText={!isDescriptionValid && "This field is required"}
             label="Description"
             onChange={(e) => setDescription(e.target.value)}
           />

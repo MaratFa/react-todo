355116a28d903bca4ba3f75e2ad7852bb26356d5
MaratFa <evolution99@rambler.ru>
2025-09-02 00:07:37 +0300

    edited

--- src/components/AddBtn/AddBtn.jsx
+++ src/components/AddBtn/AddBtn.jsx
@@ -1,16 +1,15 @@
 import classes from "./AddBtn.module.css";
 import Dialog from "@mui/material/Dialog";
 import AddIcon from "@mui/icons-material/Add";
-import AddForm from "../AddForm/AddForm";
 import { useState } from "react";
 
-export default function AddBtn({ addTask }) {
+export default function AddBtn({ children }) {
   const [isModal, setIsModal] = useState(false);
 
   return (
     <>
       <Dialog open={isModal} onClose={() => setIsModal(false)}>
-        <AddForm addTask={addTask} />
+        {children(() => setIsModal(false))}
       </Dialog>
       <div onClick={() => setIsModal(true)} className={classes["add-task-btn"]}>
         <AddIcon sx={{ width: 30, color: "white" }} />

78361e256c8f5ea99ca7e34858a3dc4880d0c52a
MaratFa <evolution99@rambler.ru>
2025-09-02 00:07:34 +0300

    edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import TaskList from "./components/TaskList/TaskList";
+import AddBtn from "./components/AddBtn/AddBtn";
+import AddForm from "./components/AddForm/AddForm";
 import { useState } from "react";
 import "./App.css";
-import AddBtn from "./components/AddBtn/AddBtn";
 
 function App() {
-  const [tasks, setTask] = useState([
+  const [tasks, setTasks] = useState([
     { id: 1, title: "Task 1", description: "Task 1 description", done: false },
     { id: 2, title: "Task 2", description: "Task 2 description", done: false },
     { id: 3, title: "Task 3", description: "Task 3 description", done: false },
@@ -12,19 +13,32 @@ function App() {
   ]);
 
   const onAddTask = (data) => {
-    setTask([...tasks, {
-      id: (tasks.at(-1)?.id + 1) || 1,
-      ...data
-    }])
-  }
+    setTasks([
+      ...tasks,
+      {
+        id: tasks.at(-1)?.id + 1 || 1,
+        ...data,
+      },
+    ]);
+  };
+
+  const onUpdateTask = (id, data) => {
+    setTasks(
+      [...tasks].map((task) => (task.id === id ? { ...task, ...data } : task))
+    );
+  };
 
   return (
     <>
       <div className="main-wrapper">
-        <TaskList title="Active" tasks={tasks} />
-        <TaskList title="Completed" tasks={tasks} />
-        <AddBtn addTask={onAddTask}/>
+        <TaskList title="Active" tasks={tasks} changeTask={onUpdateTask} />
+        <TaskList title="Completed" tasks={tasks} changeTask={onUpdateTask} />
       </div>
+      <AddBtn>
+        {(closeModal) => {
+          return <AddForm closeModal={closeModal} addTask={onAddTask} />;
+        }}
+      </AddBtn>
     </>
   );
 }

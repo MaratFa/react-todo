ea066499bddfe3672d2a2698976cb55fba921c0c
MaratFa <evolution99@rambler.ru>
2025-09-03 07:02:32 +0300

    added

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,16 @@
+import classes from "./Search.module.css";
+import TextField from "@mui/material/TextField";
+
+
+
+
+
+
+
+export default function Search() {
+  return(
+    <div className={classes.search}>
+
+    </div>
+  )
+}

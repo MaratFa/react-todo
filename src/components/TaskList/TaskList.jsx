df3dff769b3bc17a2c78e59de4ef162edcfa11ea
MaratFa <evolution99@rambler.ru>
2025-09-03 07:02:28 +0300

    edited

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,16 +1,37 @@
+import { useState, useEffect } from "react";
 import Task from "../Task/Task";
 import classes from "./TaskList.module.css";
 
-export default function TaskList({ title, tasks, changeTask }) {
-  console.log("Tasks: ", tasks);
-  
+export default function TaskList({
+  title,
+  tasks,
+  sortBy,
+  changeTask,
+  deleteTask,
+}) {
+  const [sortTasks, setSortTasks] = useState([]);
+
+  useEffect(() => {
+    setSortTasks(
+      [...tasks].filter((task) => (sortBy ? task[sortBy] : !task.done))
+    );
+  }, [tasks, sortBy]);
+  // console.log("Tasks: ", tasks);
+
   return (
     <div className={classes.wrapper}>
       <div className={classes["list-title"]}>{title}</div>
       <div className={classes["task-list"]}>
-        {tasks.length
-          ? tasks.map((task) => {
-              return <Task task={task} key={task.id} changeTask={changeTask} />;
+        {sortTasks.length
+          ? sortTasks.map((task) => {
+              return (
+                <Task
+                  task={task}
+                  key={`${task.title}-${task.done}-${task.id}`}
+                  changeTask={changeTask}
+                  deleteTask={deleteTask}
+                />
+              );
             })
           : "The task list is empty"}
       </div>

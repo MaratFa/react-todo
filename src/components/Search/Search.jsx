1cb28b0f00a012a2f95431a13f2785e254125387
MaratFa <evolution99@rambler.ru>
2025-09-04 04:57:11 +0300

    edited

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,16 +1,48 @@
 import classes from "./Search.module.css";
 import TextField from "@mui/material/TextField";
+import { useEffect, useState } from "react";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
 
+export default function Search({setSearchGlobal}) {  
+  const [searchText, setSearchText] = useState("");
+  const [isClear, setIsClear] = useState(false);
 
+  useEffect(() => {
+    if (searchText) {
+      setSearchGlobal(searchText);
+      setIsClear(true);
+      return;
+    }
 
+    setIsClear(false);
+  }, [searchText]);
 
+  const onClearClick = () => {
+    searchText("");
+    setIsClear(false);
+    setSearchText("");
+  };
 
-
-
-export default function Search() {
-  return(
+  return (
     <div className={classes.search}>
-
+      <TextField
+        label="Search"
+        variant="outlined"
+        fullWidth
+        value={searchText}
+        onInput={(e) => setSearchText(e.target.value)}
+      />
+      {isClear && (
+        <IconButton
+          onClick={() => {
+            onClearClick();
+          }}
+          className={classes.clear}
+        >
+          <CloseIcon />
+        </IconButton>
+      )}
     </div>
-  )
+  );
 }

8f353be76c005afba6a30a6a950f6e586a3908f8
MaratFa <evolution99@rambler.ru>
2025-09-07 03:00:04 +0300

    edited

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,3 +1,10 @@
+import classes from "../Notification/Notification.module.css";
+import { Alert } from "@mui/material";
 
-
-
+export default function Notification({ noti }) {
+  return (
+    <div className={classes.noti}>
+      <Alert severity={noti.type}>{noti?.text ?? "Noti"}</Alert>
+    </div>
+  );
+}

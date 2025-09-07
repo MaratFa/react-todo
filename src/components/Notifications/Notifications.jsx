7e17599de4770310c59e12901e71c76329f199e4
MaratFa <evolution99@rambler.ru>
2025-09-07 03:00:08 +0300

    edited

--- src/components/Notifications/Notifications.jsx
+++ src/components/Notifications/Notifications.jsx
@@ -1,11 +1,14 @@
 import classes from "./Notifications.module.css";
+import Notification from "../Notification/Notification";
 
 export default function Notifications({ notifications }) {
+  console.log(notifications);
+  
   return (
     <div className={classes["noti-wrapper"]}>
       {notifications &&
         notifications.map((noti) => {
-          return <div key={noti.id}>{noti.text}</div>;
+          return <Notification key={`Noti-${noti.id}`} noti={noti} />;
         })}
     </div>
   );
